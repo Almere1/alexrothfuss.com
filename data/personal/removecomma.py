e138023660f858e005eb17e6c06a8da102382e28
Almere1 <akrothfuss@gmail.com>
2021-07-27 22:19:40 -0400

    removed helper file

--- data/personal/removecomma.py
+++
@@ -1,9 +0,0 @@
-with open('personal.csv', 'r') as file :
-  filedata = file.read()
-
-# Replace the target string
-filedata = filedata.replace('，', ', ')
-
-# Write the file out again
-with open('personal.csv', 'w') as file:
-  file.write(filedata)
